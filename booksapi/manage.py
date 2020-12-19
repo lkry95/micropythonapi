66539ebf87d4f550ed2ff2ab8dcf22ee3a85c5ff
luke <lkry95@gmail.com>
2020-12-19 13:55:25 -0500

    Update fields

--- booksapi/manage.py
+++ booksapi/manage.py
@@ -17,6 +17,14 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+    with open(path) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            _, created = Teacher.objects.get_or_create(
+                first_name=row[0],
+                last_name=row[1],
+                middle_name=row[2],
+            )
 
 if __name__ == '__main__':
     main()
